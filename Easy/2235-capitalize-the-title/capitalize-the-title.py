d70cf33f8b4ac5eca4fcf1f66ec7c86b511212b7
VIPIN K <32540673+vipinkvpk@users.noreply.github.com>
2024-08-21 21:12:53 +0530

    Time: 30 ms (90.72%) | Memory: 16.6 MB (36.64%) - LeetSync

--- Easy/2235-capitalize-the-title/capitalize-the-title.py
+++ Easy/2235-capitalize-the-title/capitalize-the-title.py
@@ -3,6 +3,6 @@ class Solution:
         output = list()
         word_arr = title.split()
         for word in word_arr:
-            output.append(word.title()) if len(word) > 2 else output .append(word.lower())
+            output.append(word.title()) if len(word) > 2 else output.append(word.lower())
         return " ".join(output)
         
