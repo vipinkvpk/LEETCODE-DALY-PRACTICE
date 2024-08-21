57ab8ed667a98b05312fdb7b07d81043787d467a
VIPIN K <32540673+vipinkvpk@users.noreply.github.com>
2024-08-21 21:07:46 +0530

    Time: 37 ms (45.29%) | Memory: 16.5 MB (36.64%) - LeetSync

---
+++ Easy/2235-capitalize-the-title/capitalize-the-title.py
@@ -0,0 +1,8 @@
+class Solution:
+    def capitalizeTitle(self, title: str) -> str:
+        output = list()
+        word_arr = title.split()
+        for word in word_arr:
+            output.append(word.title()) if len(word) > 2 else output .append(word.lower())
+        return " ".join(output)
+        
