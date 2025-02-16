ccbcd7d3b390a99d1acf4ff0c57413908e2a3756
VIPIN K <32540673+vipinkvpk@users.noreply.github.com>
2025-02-16 10:54:59 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (91.29%) - LeetSync

--- Easy/3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.py
+++ Easy/3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.py
@@ -1,15 +1,28 @@
+# class Solution:
+#     def differenceOfSums(self, n: int, m: int) -> int:
+        # num1=0
+        # num2=0
+        # for i in range(n+1):
+        #     if (i%m) != 0:
+        #         num1+=i
+        #     else:
+        #         num2+=i
+        #     sum=num1-num2
+        # return sum
+
+
+
+
+#====================
+
+#Optimized
+
 class Solution:
     def differenceOfSums(self, n: int, m: int) -> int:
-        num1=0
-        num2=0
-        for i in range(n+1):
-            if (i%m) != 0:
-                num1+=i
-            else:
-                num2+=i
-            sum=num1-num2
-        return sum
-
+        total_sum = (n * (n + 1)) // 2
+        k = n // m
+        divisible_sum = m * (k * (k + 1)) // 2
+        return total_sum - 2 * divisible_sum
 
 
         
