e57f1f11fcf224953e1379822d626cd15ea3698f
VIPIN K <32540673+vipinkvpk@users.noreply.github.com>
2025-02-16 11:01:31 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (91.29%) - LeetSync

--- Easy/3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.py
+++ Easy/3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.py
@@ -1,14 +1,14 @@
-# class Solution:
-#     def differenceOfSums(self, n: int, m: int) -> int:
-        # num1=0
-        # num2=0
-        # for i in range(n+1):
-        #     if (i%m) != 0:
-        #         num1+=i
-        #     else:
-        #         num2+=i
-        #     sum=num1-num2
-        # return sum
+class Solution:
+    def differenceOfSums(self, n: int, m: int) -> int:
+        num1=0
+        num2=0
+        for i in range(n+1):
+            if (i%m) != 0:
+                num1+=i
+            else:
+                num2+=i
+            sum=num1-num2
+        return sum
 
 
 
@@ -17,12 +17,12 @@
 
 #Optimized
 
-class Solution:
-    def differenceOfSums(self, n: int, m: int) -> int:
-        total_sum = (n * (n + 1)) // 2
-        k = n // m
-        divisible_sum = m * (k * (k + 1)) // 2
-        return total_sum - 2 * divisible_sum
+# class Solution:
+#     def differenceOfSums(self, n: int, m: int) -> int:
+#         total_sum = (n * (n + 1)) // 2
+#         k = n // m
+#         divisible_sum = m * (k * (k + 1)) // 2
+#         return total_sum - 2 * divisible_sum
 
 
         
