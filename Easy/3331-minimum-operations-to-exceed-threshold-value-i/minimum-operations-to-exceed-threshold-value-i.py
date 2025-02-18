51a4d5e3dbefed55706981af0dbbfca126db5f73
VIPIN K <32540673+vipinkvpk@users.noreply.github.com>
2025-02-18 18:04:45 +0530

    Time: 3 ms (15.74%) | Memory: 17.8 MB (56.01%) - LeetSync

--- Easy/3331-minimum-operations-to-exceed-threshold-value-i/minimum-operations-to-exceed-threshold-value-i.py
+++ Easy/3331-minimum-operations-to-exceed-threshold-value-i/minimum-operations-to-exceed-threshold-value-i.py
@@ -1,12 +1,14 @@
-# class Solution:
-#     def minOperations(self, nums: List[int], k: int) -> int:
-#         count=0
-#         for i in nums:
-#             if i < k:
-#                 count+=1
-#         return count
-
 class Solution:
     def minOperations(self, nums: List[int], k: int) -> int:
-        return sum(1 for i in nums if i<k)
+        count=0
+        for i in nums:
+            if i < k:
+                count+=1
+        return count
+
+# using list comprehension
+
+# class Solution:
+#     def minOperations(self, nums: List[int], k: int) -> int:
+#         return sum(1 for i in nums if i<k)
         
