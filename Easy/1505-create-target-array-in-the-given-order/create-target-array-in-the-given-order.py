ce93b6e0b2dc1ce6fa52d73f78c37d8ab8edb677
VIPIN K <32540673+vipinkvpk@users.noreply.github.com>
2025-02-21 18:31:42 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (82.41%) - LeetSync

--- Easy/1505-create-target-array-in-the-given-order/create-target-array-in-the-given-order.py
+++ Easy/1505-create-target-array-in-the-given-order/create-target-array-in-the-given-order.py
@@ -1,9 +1,21 @@
+# COMPLEXITY-O(N*2)
 class Solution:
     def createTargetArray(self, nums: List[int], index: List[int]) -> List[int]:
         tar=[]
         for i in range(len(nums)):
             for i in range(len(index)):
-                
                 tar.insert(index[i],nums[i])
             return tar
+
+
+# ++++++++++++++++++++++++++++++
+#COMPLEXITY -O(N)
+# class Solution:
+#     def createTargetArray(self, nums: List[int], index: List[int]) -> List[int]:
+#         tar=[]
+#         for i in range(len(nums)):
+#             # for i in range(len(index)):
+                
+#             tar.insert(index[i],nums[i])
+#         return tar
         
