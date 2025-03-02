abc7b3bb057d8820ce74597125d860ce0c95f4a0
VIPIN K <32540673+vipinkvpk@users.noreply.github.com>
2025-03-02 18:35:05 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (33.44%) - LeetSync

--- Easy/27-remove-element/remove-element.py
+++ Easy/27-remove-element/remove-element.py
@@ -1,10 +1,13 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        k=0
-        for i in range(len(nums)):
+        k = 0  # Slow pointer
+
+        for i in range(len(nums)):  # Fast pointer iterates over the array
             if nums[i] != val:
-                nums[k]=nums[i]
-                k+=1
-        return k
+                nums[k] = nums[i]  # Move non-val element to the front
+                k += 1  # Move slow pointer ahead
+
+        return k  # k is the count of remaining elements
+
         
         
