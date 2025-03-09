33d02ddce7542066e043831172e001b9ca0a89c7
VIPIN K <32540673+vipinkvpk@users.noreply.github.com>
2025-03-09 20:51:46 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (62.97%) - LeetSync

--- Easy/1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.py
+++ Easy/1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.py
@@ -1,18 +1,25 @@
+# class Solution:
+#     def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
+#         # for word in word1:
+#         #     comb1="".join(word1)
+
+#         # for word in word2:
+#         #     comb2="".join(word2)
+#         comb1=""
+#         for word in word1:
+#             comb1+=word
+#         comb2=""
+#         for word in word2:
+#             comb2+=word
+#         if comb1==comb2:
+#             return True
+#         else:
+#             return False
+
+
 class Solution:
     def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
-        # for word in word1:
-        #     comb1="".join(word1)
+        return "".join(word1)=="".join(word2)
 
-        # for word in word2:
-        #     comb2="".join(word2)
-        comb1=""
-        for word in word1:
-            comb1+=word
-        comb2=""
-        for word in word2:
-            comb2+=word
-        if comb1==comb2:
-            return True
-        else:
-            return False
+        
         
