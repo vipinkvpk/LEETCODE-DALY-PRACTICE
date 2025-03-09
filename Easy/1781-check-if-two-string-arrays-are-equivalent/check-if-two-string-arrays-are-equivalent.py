99f51472cb2e6e3864b94f9f263c5682debf9941
VIPIN K <32540673+vipinkvpk@users.noreply.github.com>
2025-03-09 20:59:06 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (62.97%) - LeetSync

--- Easy/1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.py
+++ Easy/1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.py
@@ -1,25 +1,26 @@
-# class Solution:
-#     def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
+class Solution:
+    def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
 #         # for word in word1:
 #         #     comb1="".join(word1)
 
 #         # for word in word2:
 #         #     comb2="".join(word2)
-#         comb1=""
-#         for word in word1:
-#             comb1+=word
-#         comb2=""
-#         for word in word2:
-#             comb2+=word
-#         if comb1==comb2:
-#             return True
-#         else:
-#             return False
+        combined_string_1 = ""
+        for individual_word in word1:
+            combined_string_1 += individual_word
+
+        combined_string_2 = ""
+        for individual_word in word2:
+            combined_string_2 += individual_word
+
+        return combined_string_1 == combined_string_2
 
 
-class Solution:
-    def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
-        return "".join(word1)=="".join(word2)
+
+
+# class Solution:
+#     def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
+#         return "".join(word1)=="".join(word2)
 
         
         
