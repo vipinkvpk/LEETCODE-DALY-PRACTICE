97e6e625c92346b5b6eb00ef68ae082eb907afc6
VIPIN K <32540673+vipinkvpk@users.noreply.github.com>
2025-05-01 13:58:20 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (25.60%) - LeetSync

--- Easy/434-number-of-segments-in-a-string/number-of-segments-in-a-string.py
+++ Easy/434-number-of-segments-in-a-string/number-of-segments-in-a-string.py
@@ -1,9 +1,20 @@
 class Solution:
     def countSegments(self, s: str) -> int:
-        count=0
-        string1=s.split()
-        for ch in string1:
-            count+=1 
-        return count
+        return len(s.split())
 
         
+
+
+
+
+
+
+
+# BRUTE FORCE
+# class Solution:
+#     def countSegments(self, s: str) -> int:
+#         count=0
+#         string1=s.split()
+#         for ch in string1:
+#             count+=1 
+#         return count
