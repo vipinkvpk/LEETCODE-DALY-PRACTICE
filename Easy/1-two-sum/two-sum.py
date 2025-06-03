3657c92ccc170c7e2d9aa9dc5de27bfbe56c4fe4
VIPIN K <32540673+vipinkvpk@users.noreply.github.com>
2025-06-04 05:03:59 +0530

    Time: 3 ms (50.78%) | Memory: 19.1 MB (24.06%) - LeetSync

--- Easy/1-two-sum/two-sum.py
+++ Easy/1-two-sum/two-sum.py
@@ -1,8 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i,j]
-
-            
+        hash_map={}
+        for i,num in enumerate(nums):
+            complement=target-num 
+            if complement in hash_map:
+                return [hash_map[complement],i]
+            hash_map[num]=i 
+        return []
+        
