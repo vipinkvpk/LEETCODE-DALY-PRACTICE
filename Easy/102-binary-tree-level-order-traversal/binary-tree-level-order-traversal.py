5014cd1c1225877481efcd651a4ce50f0f65fc65
VIPIN K <32540673+vipinkvpk@users.noreply.github.com>
2025-08-01 00:46:07 +0530

    Time: 0 ms (100.00%) | Memory: 18.5 MB (59.81%) - LeetSync

--- Easy/102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
+++ Easy/102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
@@ -24,28 +24,50 @@ class Solution:
         #             queue.append(current_node.right) 
         #     results.append(level)
         # return results
-        if not root:
-            return []  # If the tree is empty, return an empty list
 
-        queue = deque([root])  # Start with the root node in the queue
-        result = []            # This will store the final level order traversal
+
+        # if not root:
+        #     return []  # If the tree is empty, return an empty list
+
+        # queue = deque([root])  # Start with the root node in the queue
+        # result = []            # This will store the final level order traversal
+
+        # while queue:
+        #     level = []  # List to store values of the current level
+        #     level_size = len(queue)  # Number of nodes at the current level
+
+        #     for _ in range(level_size):
+        #         current_node = queue.popleft()  # Get the front node from the queue
+        #         level.append(current_node.val)  # Add its value to the current level list
+
+        #         # Add left and right children to the queue if they exist
+        #         if current_node.left:
+        #             queue.append(current_node.left)
+        #         if current_node.right:
+        #             queue.append(current_node.right)
+
+        #     result.append(level)  # Add the completed level to the result
+
+        # return result
+
+        
+
+        if not root:
+            return []
+        queue=deque([root])
+        result = []
 
         while queue:
-            level = []  # List to store values of the current level
-            level_size = len(queue)  # Number of nodes at the current level
+            level = []
+            level_size = len(queue)
 
             for _ in range(level_size):
-                current_node = queue.popleft()  # Get the front node from the queue
-                level.append(current_node.val)  # Add its value to the current level list
+                current_node = queue.popleft()
+                level.append(current_node.val)
 
-                # Add left and right children to the queue if they exist
                 if current_node.left:
                     queue.append(current_node.left)
                 if current_node.right:
                     queue.append(current_node.right)
-
-            result.append(level)  # Add the completed level to the result
-
+            result.append(level)
         return result
-
-        
