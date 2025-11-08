4f806017af43395f034ba69e56161fc3d6b2d0cd
VIPIN K <32540673+vipinkvpk@users.noreply.github.com>
2025-11-08 12:01:17 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (19.14%) - LeetSync

--- Easy/1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.py
+++ Easy/1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.py
@@ -1,26 +1,12 @@
 class Solution:
     def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
-#         # for word in word1:
-#         #     comb1="".join(word1)
-
-#         # for word in word2:
-#         #     comb2="".join(word2)
-        combined_string_1 = ""
-        for individual_word in word1:
-            combined_string_1 += individual_word
-
-        combined_string_2 = ""
-        for individual_word in word2:
-            combined_string_2 += individual_word
-
-        return combined_string_1 == combined_string_2
-
-
-
-
-# class Solution:
-#     def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
-#         return "".join(word1)=="".join(word2)
-
-        
-        
+        str1=""
+        str2=""
+        for char in word1:
+            str1+=char
+        for ch in word2:
+            str2+=ch
+        if str1== str2:
+            return True
+        else:
+            return False
