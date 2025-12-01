eede906bbfd4a9852f58379d19fe736ec3370ab1
VIPIN K <32540673+vipinkvpk@users.noreply.github.com>
2025-12-01 20:46:48 +0530

    Time: 15 ms (29.42%) | Memory: 42.9 MB (5.83%) - LeetSync

--- Easy/2634-minimum-common-value/minimum-common-value.py
+++ Easy/2634-minimum-common-value/minimum-common-value.py
@@ -1,19 +1,12 @@
 class Solution:
     def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
-        # common=[]
-        # for i in nums1:
-        #     if i in nums2:
-        #         common.append(i)
-        # return min(common)
-        common=list(set(nums1) & set(nums2))
-        if not common:
-            return -1
+        # common = []
+        # for num in nums1:
+        #     if num in nums2:
+        #     # common.append(num)
+        #         return num
+        inter = set(nums1).intersection(set(nums2))
+        if len(inter)>0:
+            return min(inter)
         else:
-            return min(common)
-            
-
-        
-            
-
-                
-        
+            return -1
