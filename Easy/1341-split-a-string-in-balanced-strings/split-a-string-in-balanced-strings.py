580206d5efa2d841417e233ccd7bce2316d026e0
VIPIN K <32540673+vipinkvpk@users.noreply.github.com>
2026-01-19 01:07:53 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (10.36%) - LeetSync

--- Easy/1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.py
+++ Easy/1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.py
@@ -1,14 +1,14 @@
 class Solution:
     def balancedStringSplit(self, s: str) -> int:
-        w_count = l_count = r_count = 0
+        bal_count = l_count = r_count = 0
         for ch in s:
             if ch == "L":
                 l_count+=1
             else:
                 r_count += 1
             if l_count == r_count:
-                w_count += 1
-        return w_count
+                bal_count += 1
+        return bal_count
 
 
         
