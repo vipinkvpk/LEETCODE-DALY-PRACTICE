b63fc8c82201be1be526984ec6f9ace1c2c10b6e
VIPIN K <32540673+vipinkvpk@users.noreply.github.com>
2026-01-19 18:07:41 +0530

    Time: 1 ms (23.62%) | Memory: 19.5 MB (10.71%) - LeetSync

--- Easy/3567-convert-date-to-binary/convert-date-to-binary.py
+++ Easy/3567-convert-date-to-binary/convert-date-to-binary.py
@@ -4,14 +4,10 @@ class Solution:
         # binary_string_no_prefix = bin(number)[2:]
         # print(binary_string_no_prefix)
         lst_date = date.split("-")
-        str_date= ""
         binary_list=[]
         for date in lst_date:
-            
             int_date = int(date)
             binary_date = bin(int_date)[2:]
-            # for date in binary_date:
-            
             binary_list.append(binary_date)
         return "-".join(binary_list)
         
