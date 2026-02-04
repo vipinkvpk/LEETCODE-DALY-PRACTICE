7b2aabfc77c506db4e438d677d68f1e6cddd9b67
VIPIN K <32540673+vipinkvpk@users.noreply.github.com>
2026-02-04 13:14:41 +0530

    Time: 7 ms (41.63%) | Memory: 19.2 MB (42.86%) - LeetSync

---
+++ Easy/3606-minimum-element-after-replacement-with-digit-sum/minimum-element-after-replacement-with-digit-sum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minElement(self, nums: List[int]) -> int:
+        # sum=0
+        # sum_list
+        # for i in str(nums):
+        #     sum=in
+        return min(sum(map(int,str(num))) for num in nums)
+
+        
