02efa85415e4d374b795adc293522a501955eda8
VIPIN K <32540673+vipinkvpk@users.noreply.github.com>
2026-02-10 20:38:06 +0530

    Time: 39 ms (4.59%) | Memory: 19.5 MB (11.69%) - LeetSync

--- Easy/3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.py
+++ Easy/3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.py
@@ -1,5 +1,22 @@
 class Solution:
     def gcdOfOddEvenSums(self, n: int) -> int:
-        return n
+        # return n
+        odd_num=1
+        odd_sum=0 
+        for i in range(n):
+            odd_sum+=odd_num 
+            odd_num+=2 
+        
+        even_num=2
+        even_sum=0
+        for i in range(n):
+            even_sum+=even_num
+            even_num+= 2
+
+        def gcd(a,b):
+            while b:
+                a,b = b, a%b
+            return a 
+        return gcd(odd_sum, even_sum)
             
         
