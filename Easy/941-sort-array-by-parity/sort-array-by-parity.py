cce39d10e7e9a4cae973bca16a2cd94020b79feb
VIPIN K <32540673+vipinkvpk@users.noreply.github.com>
2026-03-02 10:34:54 +0530

    Time: 0 ms (100.00%) | Memory: 19.7 MB (85.62%) - LeetSync

---
+++ Easy/941-sort-array-by-parity/sort-array-by-parity.py
@@ -0,0 +1,13 @@
+class Solution:
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        left = 0
+        right = len(nums)-1 
+        while left<right:
+            if nums[left]%2 == 0:
+                left += 1
+            elif nums[right] % 2 == 1:
+                right -= 1 
+            else:
+                nums[left], nums[right]= nums[right], nums[left]
+        return nums
+        
