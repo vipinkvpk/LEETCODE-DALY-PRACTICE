22c04fe832083d6b59b0a0a7963ff9df9d390f42
VIPIN K <32540673+vipinkvpk@users.noreply.github.com>
2026-03-14 22:42:44 +0530

    Time: 3 ms (74.02%) | Memory: 19.3 MB (69.58%) - LeetSync

---
+++ Easy/2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.py
@@ -0,0 +1,13 @@
+class Solution:
+    def divideArray(self, nums: List[int]) -> bool:
+        # from itertools import pairwise
+        # is_pair = all((a,b) for a,b in pairwise(nums))
+        # return is_pair
+
+        count = Counter(nums)
+        # for v in count.values():
+        #     if v%2==0:
+        #         return True 
+        #     return False
+        return all(v%2==0 for v in count.values())
+        
