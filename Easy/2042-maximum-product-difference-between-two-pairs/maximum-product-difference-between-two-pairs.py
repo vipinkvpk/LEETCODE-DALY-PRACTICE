6f779e440303220ce62af0a342ed6ba894997a6a
VIPIN K <32540673+vipinkvpk@users.noreply.github.com>
2026-03-21 20:44:34 +0530

    Time: 144 ms (48.57%) | Memory: 20.8 MB (39.52%) - LeetSync

--- Easy/2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.py
+++ Easy/2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.py
@@ -1,7 +1,5 @@
 class Solution:
     def maxProductDifference(self, nums: List[int]) -> int:
-        # from itertools import pairwise 
-        # return max((a*b)-(c*d) for (a,b)(c,d) in pairwise(nums))
         nums.sort()
         max_product = nums[-1] * nums[-2]
         min_product = nums[0] * nums[1]
